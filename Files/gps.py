0657b57aeaffeb898c6fbe3c2bd77f258dd1ef6b
Raphael24 <raphael24@gmx.ch>
2021-11-09 11:37:12 +0100

    new Dictionary for results

--- Files/gps.py
+++ Files/gps.py
@@ -16,8 +16,23 @@ import socket
 import sys
 import os
 import time
+import urllib.request
 print('Start GPS Test')
 
+
+# Variablen
+
+result = {
+            "MAC_Adresse" : "",
+            "Latitude" : "",
+            "Longitude" : "",
+            "Speed" : "",
+            "Time" : "",
+            "GSM" : "",
+            "ETH1" : "",
+            "ETH2" : "",
+            "Memory" : "",
+}
 
 def init_gps():
     with serial.Serial('/dev/ttyUSB3', baudrate=9600, timeout=5) as ser:
@@ -37,6 +52,7 @@ def start_deamon():
 
 
 def connect_gps_client():
+    print("----- Start GPS test -----")
     client = GPSDClient(host="127.0.0.1")
     test_beenden = False
 
@@ -57,10 +73,49 @@ def connect_gps_client():
                     if latitude != 0.0 and longitude != 0.0:
                         print('Standort gefunden -> Test wird beendet')
                         test_beenden = True
+                        result["Latitude"] = str(latitude)
+                        result["Longitude"] = str(longitude)
+                        result["Speed"] = str(result.get("speed"))
+                        result["Time"] = str(result.get("time"))
+
                         break
 
                     else:
-                        print('Daten nicht gültig -> warten bis gps initialisiert ist.')
+                        print('Daten sind gültig es wurde aber kein Standort gefunden')
+
+                else:
+                    print('Daten nicht gültig -> warten bis gps initialisiert ist.')
+
+
+def check_ping():
+    print("----- Start ping -----")
+    try:
+        urllib.request.open('https://www.stadlerrail.com/de/')
+        print('URL OK')
+        os.system('ping stadlerrail.com')
+        time.sleep(5)
+        #os.system('sudo cmd .') -> überprüfen
+        print('Ping OK')
+        result["GSM"] = "OK"
+        print('Test finished')
+    except:
+        print('URL fail: No connection -> Check sim and repeat test')
+
+
+def memory_test():
+    print("----- Start memory test -----")
+    #os.system('sudo hdparm -t /dev/sda')
+    cmd = os.popen('sudo hdparm -t /dev/sda')
+    result["Memory"] = cmd[2]
+
+
+def show_macaddr():
+    print("----- Show MAC-Adresse -----")
+    os.system('ip -brief link')
+
+def get_result():
+    for i in result:
+        print(i)
 
 
 
@@ -68,3 +123,5 @@ if __name__ == '__main__':
     init_gps()
     start_deamon()
     connect_gps_client()
+    check_ping()
+    get_result()
