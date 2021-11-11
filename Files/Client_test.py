f48befa5207025c69775d2134749ddd5ff21e412
Raphael24 <raphael24@gmx.ch>
2021-11-11 11:38:25 +0100

    add parameter ip

--- Files/Client_test.py
+++ Files/Client_test.py
@@ -4,9 +4,10 @@ import time
 def test_client():
     print('test client')
 
-def ETH_client():
+def ETH_client(ip):
     print('Client Init')
-    ip = "192.168.2.101"
+    #ip = "192.168.2.101"
+    ip = ip
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, 50001))
     try:
@@ -17,6 +18,11 @@ def ETH_client():
             print("[{}] {}".format(ip, antwort.decode()))
             if antwort.decode() == "works":
                 print("success")
+                return "OK"
+                break
+            else:
+                print("Conection_Error")
+                return "NOK"
                 break
     finally:
         s.close()
